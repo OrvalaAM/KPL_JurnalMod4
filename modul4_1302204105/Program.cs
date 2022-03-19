6d988b645c7020e61783806dda5fc72d91437503
OrvalaAM <orvalavala@gmail.com>
2022-03-19 17:13:57 +0700

    menambahkan class SimpleDataBase

--- modul4_1302204105/Program.cs
+++ modul4_1302204105/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace modul4_1302204105
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SimpleDataBase<double> basdat = new SimpleDataBase<double>();
+            basdat.AddNewData(13);
+            basdat.AddNewData(02);
+            basdat.AddNewData(20);
+            basdat.PrintAllData();
+        }
+    }
+
+    public class SimpleDataBase<T>
+    {
+        List<T> storedData;
+        List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            DateTime now = DateTime.Now;
+            inputDates.Add(now);
+        }
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + (i + 1) + " berisi: " + storedData[i] + ", yang disimpan pada waktu " + inputDates[i]);
+            }
+        }
+    }
+}
